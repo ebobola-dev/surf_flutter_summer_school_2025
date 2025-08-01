5ce2eef247c6626470ace2c19f826a2ccc5ea1a6
Roma Blagov <romawot94@gmail.com>
2025-08-01 13:21:35 +0300

    chore: update Android NDK version to 27.0.1207797

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.surf_flutter_summer_school_2025"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
